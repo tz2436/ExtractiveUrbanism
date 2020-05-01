685d11faecea50c2b38bca131262d3cc3897f1da
tz2436 <tz2436@columbia.edu>
2020-04-30 21:13:20 -0400

    Update map.js

--- map.js
+++ map.js
@@ -9,8 +9,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidHoyNDM2IiwiYSI6ImNrM3pjd3ZhYjB0NWgzc3Noa282e
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v9',
-    center: [31.31197, -18.57325],
-    zoom: 8
+    center: [36.23, -18.07],
+    zoom: 6
 });
 
 var mining_url = "./data/MiningTrans.geojson"
